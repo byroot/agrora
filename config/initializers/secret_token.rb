3e27ca1c747c0e03fc9f02170c44c5c02b64f6c6
Jean Boussier <jean.boussier@dimelo.com>
2011-01-06 14:32:49 +0100

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Agrora::Application.config.secret_token = 'ebd06ab3d880c2853c3f83c7745e4edc00c3b53c830e2061c999aeee2b640b540a9731de2a10e4402400067f10afdd942b69e83110ab56c6a4f2ecedbba24df3'
