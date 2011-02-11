7e41b5e04610fd87cb9c1b984eb0f9c9bcf0ee45
Jean Boussier <jean.boussier@dimelo.com>
2011-02-11 11:22:54 +0100

    fix: User spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,13 +5,13 @@ describe User do
   subject do
     Fabricate(:user)
   end
-
+  
   it{ should have_field(:email).of_type(String) }
   it{ should have_field(:username).of_type(String) }
   it{ should have_field(:password_hash).of_type(String) }
   it{ should have_field(:password_salt).of_type(String) }
   it{ should have_field(:activation_token).of_type(String) }
-  it{ should have_field(:is_admin?).of_type(Boolean) }
+  it{ should have_field(:admin?).of_type(Boolean) }
   it{ should have_field(:state).of_type(Boolean) }
 
   it { should validate_presence_of(:email) }
@@ -21,30 +21,37 @@ describe User do
   it { should validate_uniqueness_of(:email) }
   it { should validate_format_of(:email) }
 
-  it 'should have nil as token by default' do
-    User.new.activation_token.should == nil
-  end
+  describe '.new' do
+    
+    subject do
+      User.new
+    end
+    
+    its(:activation_token) { should be_nil }
+    
+    it { should_not be_admin }
+    
+    it { should_not be_active }
+    
+   end
+  
+  describe ".authenticate" do
 
-  it 'should\'nt be admin by default' do
-    User.new.is_admin?.should == false
-  end
-
-  it 'should\'nt be active by default' do
-    User.new.active?.should == false
-  end
-
-  describe "#authentificate" do
     it "should return nil if no user match" do
-      User.authenticate("nobody@somewhere.com", "something") == nil
+      User.authenticate("nobody@somewhere.com", "something").should be_nil
     end
 
     it "should return the user if email and password match" do
-      #user is activate? in theory no
-      User.authenticate("test@example.com", "azerty") == User.find(:first, :conditions => {:email => "test@example.com"})
+      subject.activate!
+      User.authenticate("test@example.com", "azerty").should == subject
+    end
+    
+    it 'should not authenticate unactivated users' do
+      User.authenticate("test@example.com", "azerty").should be_nil
     end
     
     it "should'nt authenticate the user if the password is wrong" do
-      User.authenticate("test@example.com", "something") == nil
+      User.authenticate("test@example.com", "something").should be_nil
     end
   end
 
