90e3a27547d6fba0fe5cff01fcac05fd7aa35316
Jean Boussier <jean.boussier@dimelo.com>
2011-02-17 23:21:41 +0100

    refactor: css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,8 @@ module ApplicationHelper
     instance.class.name.underscore.dasherize
   end
   
+  def body_tag
+    tag(:body, {:id => html_class(controller), :class => "action-#{params[:action]}"}, true)
+  end
+  
 end
