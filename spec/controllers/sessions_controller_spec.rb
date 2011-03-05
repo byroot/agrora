2829706d489acec23c0d312240e4f3eebaca6ebf
Jean Boussier <jean.boussier@dimelo.com>
2011-03-05 13:35:29 +0100

    spec: improve sessions controller specs

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -6,13 +6,23 @@ describe SessionsController do
     @user = Fabricate(:user)
   end
   
+  describe '#new' do
+    
+    it 'should be success' do
+      get :new
+      response.should be_success
+    end
+    
+  end
+  
   describe '#create' do
+    
     it 'should render new if fail' do
       post :create, :email => 'test', :password => 'foo'
       controller.should render_template('new')
     end
     
-    it 'should fail if user is not activate' do
+    it 'should fail if user is not activated' do
       post :create, :email => 'test@example.com', :password => 'azerty'
       controller.should render_template('new')
     end
@@ -21,17 +31,38 @@ describe SessionsController do
       expect{
         @user.activate!
         post :create, :email => 'test@example.com', :password => 'azerty'
+        response.should be_redirect
         response.should redirect_to(groups_url)
-      }.to change{assigns(:current_user)}.from(nil).to(@user.reload)
+      }.to change{ assigns(:current_user) }.from(nil).to(@user.reload)
     end
+    
+    it 'should redirect to redirect params if success and params is present' do
+      expect{
+        @user.activate!
+        post :create, :email => 'test@example.com', :password => 'azerty', :redirect => '/foo/bar'
+        response.should be_redirect
+        response.should redirect_to('/foo/bar')
+      }.to change{ assigns(:current_user) }.from(nil).to(@user.reload)
+    end
+    
   end
 
   describe '#destroy' do
+    
+    before :each do
+      logged_in_as @user
+    end
     
     it 'should redirect to root' do
       post :destroy
       response.should be_redirect
       response.should redirect_to(root_url)
+    end
+    
+    it 'should remove :user_id from session' do
+      expect{
+        post :destroy
+      }.to change{ session[:user_id] }.from(@user.id.to_s).to(nil)
     end
     
   end
