876e0afb620b9f43251ccce1a0904001639f0b2a
RedLion973 <red_lion-973@hotmail.fr>
2011-03-07 12:34:57 +0100

    new footer design and adds actions list buttons design [uncompleted]

---
+++ public/javascripts/application.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $('#header-title').hover(
+        function() { $('#agrora-logo').attr('src','/images/agRORa_icon_inverse.png'); },
+        function() { $('#agrora-logo').attr('src','/images/agRORa_icon.png'); }
+    );
+});
